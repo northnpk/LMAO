4a6c379d98ddffe4810518fd1daa10a523b1c89c
northnpk <north2372001@gmail.com>
2024-05-06 19:36:41 +0700

    [ADD] Adding Graph.py including data preprocessing from log dataframe to X,y dataframe with 3 modes ("seq":[log1_topic, log2_topic,...], "freq":[topic1_freq, topic2_freq,...], "PyG": pytorch geometry representation)

--- lmao/graph/__init__.py
+++ lmao/graph/__init__.py
@@ -1,26 +1,69 @@
 from .._utils import *
 import networkx as nx
 import plotly.express as px
-class graph:
+from collections import Counter
+from .pyg import getting_loader, get_graph
+
+class Graph:
     def __init__(self, df:pd.DataFrame,
                  group_name:str='session_id',
                  X_col:str='topic', y_col:str='label',
-                 mode='seq', padding=True, seq_size:int=300):
+                 mode='seq', padding=True, seq_size:int=300) -> pd.DataFrame:
+        self.graph_list = []
         self.mode = mode
-        print('Getting Group by from dataframe')
-        self.df = group_to_classify(df=df, group_name=group_name,
+        self.n_topics = len(df['topic'].unique())
+        print(f'Topics size of this dataframe is : {self.n_topics}')
+        if self.mode == 'seq':
+            print('Getting Group by from dataframe')
+            self.df = group_to_classify(df=df, group_name=group_name,
                                     X_col=X_col, y_col=y_col)
-        if padding :
-            print(f'Apply Padding and Truncate {seq_size}.')
-            self.df['X'] = self.df['X'].progress_apply(pad_truncate, seq_size)
+            if padding :
+                print(f'Apply Padding and Truncate {seq_size}.')
+                self.df['X'] = self.df['X'].progress_apply(pad_truncate, max_len=seq_size)
+                self.seq_size = seq_size
+                self.n_topics = len(df['topic'].unique()) + 1 # Adding Pad (-2) to the topic input
+                print(f'Updating Topic size of this dataframe to {self.n_topics}')
         
-        if mode == 'seq':
-            return self.df
-        elif mode == 'pyg':
-            return self.df
+        elif self.mode == 'freq':
+            print('Getting Group by from dataframe')
+            self.df = group_to_classify(df=df, group_name=group_name,
+                                    X_col=X_col, y_col=y_col)
+            
+            print('Apply Count to calculate each topic frequency')
+            self.df['X'] = self.df['X'].progress_apply(get_freq, n_topics=2200)
+            
+        elif self.mode == 'pyg':
+            print('Getting Group by from dataframe')
+            self.df = group_to_classify(df=df, group_name=group_name,
+                                    X_col=X_col, y_col=y_col)
+            if padding :
+                print(f'Apply Padding and Truncate {seq_size}.')
+                self.df['X'] = self.df['X'].progress_apply(pad_truncate, max_len=seq_size)
+                self.seq_size = seq_size
+                self.n_topics = len(df['topic'].unique()) + 1 # Adding Pad (-2) to the topic input
+                print(f'Updating Topic size of this dataframe to {self.n_topics}')
+            print('Please call .get_PyG() to get PyG dataloader')
+            
         else :
             print(f'We do not have the {mode} mode yet.')
-            return self.df
+            self.df = df
+            
+    def get_networkx(self, one_hot:bool=False):
+        n_features = 2
+        if one_hot:
+            n_features+=self.seq_size
+        print('Getting networkx Graph from dataframe')
+        self.graph_list = self.df['X'].progress_apply(get_graph)
+        return self.graph_list
+    
+    def get_PyG(self, one_hot:bool=False, batch_size:int=32):
+        n_features = 2
+        if one_hot:
+            n_features+=self.seq_size
+        group_node_attrs = list(map(lambda x: str(x), range(0, n_features)))
+        print('Getting PyG Loader Graph from dataframe')
+        return getting_loader(df=self.df, group_node_attrs=group_node_attrs,
+                              batch_size=batch_size, len_seq=self.seq_size)
         
     
 def group_to_classify(df:pd.DataFrame,
@@ -44,3 +87,13 @@ def pad_truncate(seq, max_len:int=300):
         pad_length = max_len - len(seq)
         padded_sequence = np.pad(seq, (0, pad_length), mode='constant', constant_values=-2)
         return padded_sequence
+    
+def get_freq(seq, n_topics:int=2200): #topic from Alice_infologger is 2148 topics
+    topics_key = np.zeros((n_topics))
+    # print(topics_key.shape)
+    c = Counter(seq)
+    len_seq = len(seq)
+    for a, b in c.items():
+        topics_key[a] = b / len_seq
+        # print(f'{a}:{b}')
+    return topics_key
--- lmao/graph/pyg.py
+++ lmao/graph/pyg.py
@@ -10,36 +10,19 @@ from torch_geometric.utils import from_networkx
 class2bin = {'Normal':0,
              'Anomaly':1}
 bin2class = ['Normal', 'Anomaly']
+
 def class_trans(data, mode=class2bin):
     return mode[data]
 
-class PyGraph:
-    def __init__(self, df:pd.DataFrame, mode='topic_seq', seq_len:int=300):
-        df['y'] = df['y'].apply(class_trans, mode=class2bin)
-        if mode == 'topic_only':
-            self.n_features = 2
-            self.group_node_attrs = list(map(lambda x: str(x), range(0, self.n_features)))
-        elif mode == 'topic_seq':
-            self.n_features = seq_len+2
-            self.group_node_attrs = list(map(lambda x: str(x), range(0, self.n_features)))
-        
-        else :
-            print(f'We do not have the {mode} mode yet. Using topic_seq mode.')
-            self.n_features = seq_len+2
-            self.group_node_attrs = list(map(lambda x: str(x), range(0, self.n_features)))
-        
-        return 
-        
-
-def pos_onehot(seq, keys):
-    a = np.zeros((len(keys), 300))
+def pos_onehot(seq, keys, len_seq):
+    a = np.zeros((len(keys), len_seq))
     for i, n in enumerate(keys):
         pos = np.where(seq==n)[0]
         for p in pos:
             a[i][p] = 1
     return a
 
-def get_graph(seq):
+def get_graph(seq, len_seq:int=300):
     edge_list=[]
     data_size = len(seq)
     # for idx, node in tqdm(enumerate(seq), total=data_size):
@@ -50,7 +33,7 @@ def get_graph(seq):
     nodes = list(c.keys())
     # print(nodes)
     data = np.array(list(c.items()))
-    pos = pos_onehot(seq, nodes)
+    pos = pos_onehot(seq, nodes, len_seq)
     x = np.hstack([data, pos])
     # print(x)
     node_list = []
@@ -63,11 +46,12 @@ def get_graph(seq):
     G.add_edges_from(edge_list)
     return G
 
-def get_PyG_data(row, group_node_attrs):
-    G = get_graph(row['X'])
+def get_PyG_data(row, group_node_attrs, len_seq:int=300):
+    G = get_graph(row['X'], len_seq=len_seq)
     data = from_networkx(G, group_node_attrs=group_node_attrs)
     data.y = row['y']
     return data
 
-def getting_loader(df, group_node_attrs, batch_size=32):
-    return DataLoader(df.progress_apply(get_PyG_data, group_node_attrs, axis=1).to_list(), batch_size=batch_size)
+def getting_loader(df, group_node_attrs, batch_size=32, len_seq:int=300):
+    return DataLoader(df.progress_apply(get_PyG_data, group_node_attrs=group_node_attrs,
+                                        len_seq=len_seq, axis=1).to_list(), batch_size=batch_size)
